3c0171ab0cfa96cf8a5d568fe62dde8d9dfde400
billy-harness <87822493+billy-harness@users.noreply.github.com>
2021-11-05 09:51:32 -0400

    Update a_HomePage.php

--- nginx/src/a_HomePage.php
+++ nginx/src/a_HomePage.php
@@ -1,9 +1,9 @@
 <?php
 /////////////////////////
-// --- HOME PAGE ---  //
+/// --- HOME PAGE ---  ///
 ///////////////////////
 
-$auth_version=1; // app Version (1 or 2)
+$auth_version=; // app Version (1 or 2)
 $auth_icon=""; // Image (only available with version 2)
 $auth_message=""; // Text in the Landing page
 $internal_usage=""; // Put here anything you want
